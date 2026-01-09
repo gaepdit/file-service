05278f4e89ea111553f278e6cc8bf50c559da79a
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-01-09 14:23:24 -0500

    Update Azure Blobs NuGet package and update code

--- src/FileService/Implementations/AzureBlobStorage.cs
+++ src/FileService/Implementations/AzureBlobStorage.cs
@@ -46,6 +46,8 @@ public class AzureBlobStorage : IFileService
         [EnumeratorCancellation] CancellationToken token = default)
     {
         var blobItems = _containerClient.GetBlobsAsync(
+                traits: BlobTraits.None,
+                states: BlobStates.None,
                 prefix: PathTool.CombineWithDirectorySeparator(_basePath, path),
                 cancellationToken: token)
             .ConfigureAwait(false);
